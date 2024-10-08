d07fc40b0159806433e8e271ff29c7ed847cb8aa
tamourax <at074402@gmail.com>
2024-10-08 20:08:04 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ahtarandakhtar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ahtarandakhtar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/utils/AppImage.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesFooditem
+  /// assets/images/fooditem.svg
+  static const String assetsImagesFooditem = "assets/images/fooditem.svg";
+
+  /// Assets for assetsImagesHook
+  /// assets/images/hook.svg
+  static const String assetsImagesHook = "assets/images/hook.svg";
+}
+
